871271d90480c7407ed3d3ecd7f65338d499eda2
Aryan Manhas <2022a1r033@mietjammu.in>
2026-05-07 06:49:33 +0530

    fix(logger): Use @echomind/logger in server to fix build error

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,13 +1,3 @@
-import pino from 'pino';
+import { logger } from '@echomind/logger';
 
-export const logger = pino({
-  level: process.env.LOG_LEVEL || 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:standard',
-      ignore: 'pid,hostname'
-    }
-  }
-});
+export { logger };
